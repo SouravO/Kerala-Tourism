a7e9398791ee2c2365493a9c6c059f80a886e41a
SouravO <85838332+SouravO@users.noreply.github.com>
2021-11-10 22:06:06 +0530

    after password

--- signup.js
+++ signup.js
@@ -1,16 +1,19 @@
 let email = document.getElementById("mail");
 let error = document.getElementById("errorm")
 
+
+//main function
 function check(){
     let regexp = /^([\w\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
 
         if(regexp.test(email.value)){
             error.innerHTML ="valid";
-            error.style = "green";
-            return true;
+            email.style.border = "2px solid green";
+            return pcheck();
+            
 
         }
-        else{
+        else {
             error.innerHTML= "Invalid !!";
             error.style.color = "red";
             email.style.border = " 2px solid red "
@@ -18,4 +21,45 @@ function check(){
         }
         
 
+            
 }
+
+//number 
+
+let num = document.getElementById("number") ;
+function pcheck(){
+  let phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  if(phoneno.test(num.value))
+        {
+        num.style.border = "2px solid green"
+      return checkpass();
+        }
+      else
+        {
+        alert("PLease Enter a valid phone number ");
+        num.style.border ="2px solid red" 
+        return false;
+        }
+
+
+
+
+}
+
+
+//password
+let pass = document.getElementById("pd")
+function checkpass(){
+    let strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    if(strongRegex.test(pass.value)){
+        
+        alert("done")
+        return true;
+    }
+    else{
+        alert("false")
+        return false;
+
+        }
+
+    }
